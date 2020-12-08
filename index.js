5ce823f5f3a5ec85e37aaa5ae170151aa25a98b5
FelixDaCraft <el.ombre.de.la.mota@gmail.com>
2020-12-08 05:20:29 +0100

    init discord.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready',() => {
+    console.log('Ready !')
+});
+
+client.on('message', (message) => {
+    if(message.content == '!ping'){
+        message.channel.send('Pong !');
+    }
+});
+
+client.login(process.env.TOKEN);
+
