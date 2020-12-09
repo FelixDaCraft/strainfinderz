cc244bd732e20d0823179694d48e7336553f110c
FelixDaCraft <el.ombre.de.la.mota@gmail.com>
2020-12-09 06:01:29 +0100

    display parent and breeder for parent OK

---
+++ helpers.js
@@ -0,0 +1,51 @@
+
+function capitalizeWords(text) {
+    return text.replace(/(?:^|\s)\S/g, (res) => { return res.toUpperCase(); })
+};
+
+function capitalizeAll(array) {
+    let capitalizedArray = [];
+    array.forEach((element, index) => {
+        capitalizedArray[index] = capitalizeWords(element);
+    });
+    return capitalizedArray
+}
+
+function strainFormat(strain) {
+    strain = strain.replace('#', '_Nr');
+    strain = strain.split(' ');
+    strain = capitalizeAll(strain);
+    strain = strain.join('_');
+
+    return strain;
+}
+
+function breederFormat(breeder){
+    breeder = breeder.split(' ');
+    breeder = capitalizeAll(breeder);
+    breeder = breeder.join('_');
+
+    return breeder;
+}
+
+function splitSearch (search){
+        search = search.replace('!search ', '');
+        search = search.split(' // ');
+
+        return search;
+}
+
+function urlFormat (search){
+    let breeder;
+    let strain;
+
+    search = splitSearch(search);
+    console.log(search);
+    breeder = breederFormat(search[1]);
+    strain = strainFormat(search[0]);
+
+    return `https://en.seedfinder.eu/api/json/strain.json?br=${breeder}&str=${strain}&parents=1&ac=${process.env.SEED}`;
+}
+
+
+module.exports = {urlFormat};
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+
 const Discord = require('discord.js');
+const helpers = require('./helpers')
+const fetch = require('node-fetch');
+
 
 const client = new Discord.Client();
-const fetch = require('node-fetch');
+
 
 
 
@@ -12,41 +16,36 @@ client.once('ready', () => {
 client.on('message', (message) => {
     if (message.content.includes('!search') && message.content.includes('//')) {
         let strainInfo;
-        let all = message.content.split('!search ');
 
-        all.splice(0, 1);
-        all = all.toString();
-        all = all.split('// ')
+        let url = helpers.urlFormat(message.content);
+        console.log(url);
 
-        let strain = capitalizeWords(all[0]);
-        let breeder = capitalizeWords(all[1]);
-
-        strain = strain.replace('#', ' Nr');
-
-        strain = strain.replace(' ', '_');
-
-        breeder = breeder.replace(' ', '_');
-
-        let url = 'https://en.seedfinder.eu/api/json/strain.json?br=' + breeder + '&str=' + strain + '&parents=1&ac='+ process.env.SEEDS ;
-
-        fetch(url)
-            .then(res => res.json())
-            .then(json => strainInfo = json).then(() => {
-                let parents;
-                console.log(strainInfo);
-                if (strainInfo.parents.strains.aaa != undefined && strainInfo.parents.strains.bbb != undefined && strainInfo.parents.strains.ccc != undefined) {
-                    parents = strainInfo.parents.strains.aaa.name + ' x ' + strainInfo.parents.strains.bbb.name + ' x ' + strainInfo.parents.strains.ccc.name;
-                }
-                if (strainInfo.parents.strains.aaa != undefined && strainInfo.parents.strains.bbb != undefined && strainInfo.parents.strains.ccc == undefined) {
-                    parents = strainInfo.parents.strains.aaa.name + ' x ' + strainInfo.parents.strains.bbb.name;
-                }
-                if (strainInfo.parents.strains.aaa != undefined && strainInfo.parents.strains.bbb == undefined && strainInfo.parents.strains.ccc == undefined) {
-                    parents = strainInfo.parents.strains.aaa.name;
-                }
-                message.channel.send('Strain : ' + strainInfo.name + '\n'
-                + 'Breeder : ' + strainInfo.brinfo.name + '\n'
-                + 'Parent : ' + parents)
-            });
+        try {
+            fetch(url)
+                .then(res => res.json())
+                .then(json => strainInfo = json).then(() => {
+                    if (strainInfo.error === false) {
+                        let parents;
+                        console.log(strainInfo.parents.strains.aaa)
+                        if (strainInfo.parents.strains.aaa != undefined && strainInfo.parents.strains.bbb != undefined && strainInfo.parents.strains.ccc != undefined) {
+                            parents = `${strainInfo.parents.strains.aaa.name} (from ${strainInfo.parents.strains.aaa.brname}) x ${strainInfo.parents.strains.bbb.name} (from ${strainInfo.parents.strains.bbb.brname}) x ${strainInfo.parents.strains.ccc.name} (from ${strainInfo.parents.strains.ccc.brname})`;
+                        }
+                        if (strainInfo.parents.strains.aaa != undefined && strainInfo.parents.strains.bbb != undefined && strainInfo.parents.strains.ccc == undefined) {
+                            parents = `${strainInfo.parents.strains.aaa.name} (from ${strainInfo.parents.strains.aaa.brname}) x ${strainInfo.parents.strains.bbb.name} (from ${strainInfo.parents.strains.bbb.brname})`;
+                        }
+                        if (strainInfo.parents.strains.aaa != undefined && strainInfo.parents.strains.bbb == undefined && strainInfo.parents.strains.ccc == undefined) {
+                            parents = `${strainInfo.parents.strains.aaa.name} (from ${strainInfo.parents.strains.aaa.brname})`;
+                        }
+                        message.channel.send('Strain : ' + strainInfo.name + '\n'
+                            + 'Breeder : ' + strainInfo.brinfo.name + '\n'
+                            + 'Parent : ' + parents)
+                    }else{
+                        message.channel.send(strainInfo.error);
+                    }
+                });
+        } catch (error) {
+            message.content(`Une erreur c'est produite, verifie l'orthographe du breeder ou du strain :P`);
+        }
 
 
     }
@@ -54,12 +53,9 @@ client.on('message', (message) => {
 
 client.login(process.env.TOKEN);
 
-// process.env.TOKEN
+// 
 
 //fetch('https://en.seedfinder.eu/api/json/strain.json?br=Centennial_Seeds&str=Otto_Nr1&parents=1&ac=bfd10e24b41a8c503477a68f2d61c4e2').then(res => res.json())
 //.then(json =>message.channel.send(json.parents.strains.aaa.name));
 
 
-function capitalizeWords(text) {
-    return text.replace(/(?:^|\s)\S/g, (res) => { return res.toUpperCase(); })
-};
