df045aec74d0cccf2900154e60ca0121106512be
felix <el.ombre.de.la.mota@gmail.com>
2020-12-11 16:16:51 +0100

    reaction test

--- commands/search.js
+++ commands/search.js
@@ -31,6 +31,25 @@ module.exports = {
                         }
                         message.channel.send(`Strain : ${strainInfo.name}\nBreeder : ${strainInfo.brinfo.name}\nParent : ${parents}\nLink : ${urlSeed}`);
 
+                        message.react('👍').then(() => message.react('👎'));
+
+                        const filter = (reaction, user) => {
+                            return ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id;
+                        };
+
+                        message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+                            .then(collected => {
+                                const reaction = collected.first();
+
+                                if (reaction.emoji.name === '👍') {
+                                    message.reply('you reacted with a thumbs up.');
+                                } else {
+                                    message.reply('you reacted with a thumbs down.');
+                                }
+                            })
+                            .catch(collected => {
+                                message.reply('you reacted with neither a thumbs up, nor a thumbs down.');
+                            });
                     } else {
                         message.channel.send(strainInfo.error);
                     }
