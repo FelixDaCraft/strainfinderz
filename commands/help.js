1d9b0abda40abdb0b02802448c6541f1312f514d
felix <el.ombre.de.la.mota@gmail.com>
2020-12-11 13:33:50 +0100

    embed mod 3

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,11 @@
 module.exports = {
     name: 'help',
-    description: 'Search a strain on Seedfinder.com',
+    description: 'Bot Help',
     execute(message) {
 
         const MessageEmbed = require('discord.js');
 
-        const embed = new MessageEmbed().setTitle('Commandes StrainfFinder').setColor(CFE8BA).setDescription(`Pour effectuer une recherche sur seedfinder :\n!search strain // breeder`);
+        const embed = new MessageEmbed().setTitle('Commandes StrainfFinder').setColor('DARK_GREEN').setDescription(`Pour effectuer une recherche sur seedfinder :\n!search strain // breeder`);
 
         message.channel.send(embed);
     }
