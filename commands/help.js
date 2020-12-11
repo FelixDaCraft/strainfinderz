b41144aad5d5995d664993465112e748a5fbadc8
felix <el.ombre.de.la.mota@gmail.com>
2020-12-11 13:37:20 +0100

    embed mod 4

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,12 @@ module.exports = {
 
         const MessageEmbed = require('discord.js');
 
+        try {
         const embed = new MessageEmbed().setTitle('Commandes StrainfFinder').setColor('DARK_GREEN').setDescription(`Pour effectuer une recherche sur seedfinder :\n!search strain // breeder`);
+        }
+        catch(error){
+            console.log(error);
+        }
 
         message.channel.send(embed);
     }
