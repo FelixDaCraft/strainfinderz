2168c3ee867bec8bb25bacf515ba1e2d354eee27
felix <el.ombre.de.la.mota@gmail.com>
2020-12-11 12:42:59 +0100

    !help start

---
+++ commands/help.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'help',
+    description: 'Search a strain on Seedfinder.com',
+    execute(message) {
+        message.channel.send(
+            `Pour effectuer une recherche sur seedfinder :\n
+            !search strain // breeder`
+            )
+    }
+};
